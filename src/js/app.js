69f1b77cfc357b9fa471133251f075c946bb98d1
Tom <tsanchis@edenschool.fr>
2022-04-21 15:29:05 +0200

    finish KanyeQuote in JavaScript

--- src/js/app.js
+++ src/js/app.js
@@ -15,23 +15,42 @@ btn.addEventListener("click", () => {
       const divbtn = document.createElement("div");
       divbtn.className = "btn-div-flex";
       const btnup = document.createElement("button");
-      btnup.className = "btn";
+      btnup.className = "btn up";
       btnup.textContent = "up";
       const btndown = document.createElement("button");
-      btndown.className = "btn";
+      btndown.className = "btn down";
       btndown.textContent = "down";
       quotes.appendChild(divquote);
       divquote.appendChild(p);
       divquote.appendChild(divbtn);
       divbtn.appendChild(btnup);
       divbtn.appendChild(btndown);
-
-      btnup.addEventListener("click", () => {
-        console.log("up");
-      });
-
-      btndown.addEventListener("click", () => {
-        console.log("down");
-      });
+      up();
+      down();
     });
 });
+
+function up() {
+  const divblock = document.querySelectorAll(".block");
+  const buttonup = document.querySelectorAll(".up");
+  for (let i = 0; i < divblock.length; i++) {
+    buttonup[i].onclick = function () {
+      let temp = divblock[i];
+      divblock[i].previousElementSibling.insertAdjacentElement(
+        "beforebegin",
+        temp
+      );
+    };
+  }
+}
+
+function down() {
+  const divblock = document.querySelectorAll(".block");
+  const buttondown = document.querySelectorAll(".down");
+  for (let i = 0; i < divblock.length; i++) {
+    buttondown[i].onclick = function () {
+      let temp = divblock[i];
+      divblock[i].nextElementSibling.insertAdjacentElement("afterend", temp);
+    };
+  }
+}
